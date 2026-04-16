a1135747c7eccf5a58f3df33e4e3f70ffa63c54b
rsai-praveen <mailtome2737@gmail.com>
2026-04-16 22:18:11 +0530

    String Program-1

--- src/stringprograms/ReverseString.java
+++ src/stringprograms/ReverseString.java
@@ -14,10 +14,10 @@ public class ReverseString {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter the number you want to Sum number of digits : ");
+		System.out.print("Enter the String want to Reverse : ");
 		String st = sc.next();
 		sc.close();
-		System.out.println(reverse(st));
+		System.out.printf("The reverse of String is %s" ,reverse(st));
 
 	}
 
