996bd9cb516b1edea7ed6f5b2a80a4169e74db63
rsai-praveen <mailtome2737@gmail.com>
2026-04-19 15:18:07 +0530

    String Program-4

---
+++ src/stringprograms/EachChaFrequency.java
@@ -0,0 +1,39 @@
+package stringprograms;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+import java.util.Scanner;
+
+public class EachChaFrequency {
+
+	public static void frequency(String s) {
+
+		String[] Array = s.split("");
+		HashSet<String> setList = new HashSet<String>(Arrays.asList(Array));
+
+		for (String a : setList) {
+			int count = 0;
+			for (String b : Array) {
+				if (a.equals(b)) {
+					count++;
+				}
+			}
+			System.out.printf("The Frequecy of '%s'q in a array is %d", a, count);
+			System.out.println();
+		}
+
+	}
+
+	public static void main(String[] args) {
+//Find frequency of each character
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the string want to check Character Frequency: ");
+		String str = sc.nextLine();
+		sc.close();
+
+		frequency(str);
+
+	}
+
+}
