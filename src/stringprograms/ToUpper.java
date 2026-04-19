80478dcc4030355bc8fcd8e2f56ef89eb0b0af61
rsai-praveen <mailtome2737@gmail.com>
2026-04-19 15:45:09 +0530

    String Program-6

---
+++ src/stringprograms/ToUpper.java
@@ -0,0 +1,21 @@
+package stringprograms;
+
+import java.util.Scanner;
+
+public class ToUpper {
+
+	public static void main(String[] args) {
+		// TODO 6 Convert lowercase to uppercase
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the string want to convert to uppercase ");
+		String str = sc.nextLine();
+		
+
+		sc.close();
+		
+		System.out.println(str.toUpperCase());
+				
+
+	}
+
+}
