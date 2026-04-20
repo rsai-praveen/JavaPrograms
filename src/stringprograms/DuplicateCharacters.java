89407aa2df1f28a2bd44b6e4dc4d2b1a903f47a8
rsai-praveen <mailtome2737@gmail.com>
2026-04-20 22:49:49 +0530

    String Program-9

--- src/stringprograms/DuplicateCharacters.java
+++ src/stringprograms/DuplicateCharacters.java
@@ -7,39 +7,39 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class DuplicateCharacters {
-	public static HashMap<String,Integer> duplicate(String s)
-	{
-		String[] s1=s.split("");
-		HashMap<String,Integer> map=new HashMap<String, Integer>();
-		HashSet<String> set1=new HashSet<String>(Arrays.asList(s1));
-		
-		for(String a:set1)
-		{
+	public static HashMap<String, Integer> duplicate(String s) {
+		String[] s1 = s.split("");
+		HashMap<String, Integer> map = new HashMap<String, Integer>();
+		HashSet<String> set1 = new HashSet<String>(Arrays.asList(s1));
+
+		for (String a : set1) {
 			int count = 0;
-		for(int i=0;i<s.length();i++)
-		{
-			if(a.equals(s1[i]))
-			{
-				count++;
+			for (int i = 0; i < s.length(); i++) {
+				if (a.equals(s1[i])) {
+					count++;
+				}
 			}
-		}
-		map.put(a, count);
+			map.put(a, count);
 		}
 		return map;
-		
+
 	}
 
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter the string-1 want to check duplicate characters: ");
 		String str1 = sc.nextLine();
+		str1=str1.replace(" ",""); // removes the white spaces
 		System.out.println();
 		sc.close();
-		HashMap<String,Integer> map1=duplicate(str1);
-		for(Map.Entry<String,Integer> entry:map1.entrySet() )
-		{
-			System.out.println(entry.getKey()+" , "+entry.getValue());
+		HashMap<String, Integer> map1 = duplicate(str1);
+		for (Map.Entry<String, Integer> entry : map1.entrySet()) {
+			if (entry.getValue() > 1) {
+
+				System.out.println("The Character " + entry.getKey() + " is repeated for " + entry.getValue()
+						+ " in the Given String");
+			}
 		}
 
 	}
