888623bc44cb1f05cdf6ad68278a026956aaeb49
rsai-praveen <mailtome2737@gmail.com>
2026-04-25 16:47:14 +0530

    Array Program-2

---
+++ src/arrayprograms/smallestElement.java
@@ -0,0 +1,20 @@
+package arrayprograms;
+
+public class smallestElement {
+	public static int minArray(int[] a) {
+		int min = a[0];
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] < min) {
+				min = a[i];
+			}
+		}
+		return min;
+	}
+
+	public static void main(String[] args) {
+		
+		int[] a = { 1, 2, 3, 011, 9, 8, 77, 6, 5 };
+		System.out.printf("The maimum of given array is %d", minArray(a));
+	}
+
+}
